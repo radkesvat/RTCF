f161d66ea72d739c044287da02741c9c9f1e5ea1
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2024-01-07 18:11:00 +0000

    dont remove while iterating

--- src/utils/timerdispatcher.nim
+++ src/utils/timerdispatcher.nim
@@ -3,11 +3,13 @@ import tables,chronos
 
 
 
-type TimerDispatcher* = ref object
-    handles: Table[int64,proc(){.closure,raises: [].}]
+type 
+    CallBack = proc(){.closure,raises: [].}
+    TimerDispatcher* = ref object
+        handles: Table[int64,CallBack]
 
 
-proc register*(self:TimerDispatcher,cb: proc(){.closure,raises: [].}):int64=
+proc register*(self:TimerDispatcher,cb:CallBack):int64=
     var key = Moment.now().epochNanoSeconds()
     if self.handles.hasKeyOrPut(key,cb):
         # this is impossible since tested many times the hardest possible cases
@@ -22,8 +24,11 @@ proc unregister*(self:TimerDispatcher,key:int64)=
 
 proc start*(self:TimerDispatcher,delay:Duration){.async: (raises: []).}=
     while true:
+        var funcs = newSeqOfCap[CallBack](cap = self.handles.len)
+        
         for (k,v) in self.handles.pairs:
-            v()
+            funcs.add v
+        for v in funcs: v()
         try:
             await sleepAsync(delay)
         except:
